6f29994c03889f869b7c0e3a4449f4501bac8398
Niels <you@example.com>
2023-09-20 22:20:18 -0500

    Lab3 terminado

---
+++ Lab3_1.js
@@ -0,0 +1,10 @@
+var nombre = "Carlos"
+console. log(nombre);
+var numero = 1000;
+console. log (numero);
+var objecto = {
+    nombre : "Juan",
+    apellido : "Perez",
+    telefono: "5544332"
+}
+console. log(objecto);
---
+++ Lab3_2.js
@@ -0,0 +1,36 @@
+var bandera = false;
+console.log(typeof bandera);
+
+function miFuncion() {}
+console.log(typeof miFuncion);
+
+var simbolo = Symbol("mi simbolo");
+console.log(typeof simbolo);
+
+class Persona {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona);
+
+var x;
+console.log(typeof x);
+
+x = undefined;
+console.log(typeof x);
+
+var y = null;
+console.log(typeof y);
+
+var y = null;
+console.log(typeof y);
+
+var autos = ['BMW', 'Audi', 'Volvo'];
+console.log(autos);
+console.log(typeof autos);
+
+var z = '';
+console.log(z);
+console.log(typeof z);
